e31bea8360aeda309d613fbdc40df6203a61c0b3
Jonas Engelhardt <73387330+joengelh@users.noreply.github.com>
2021-02-04 21:59:43 +0100

    Update push-documents.ps1

--- push-documents.ps1
+++ push-documents.ps1
@@ -1,4 +1,3 @@
 $ErrorActionPreference = 'SilentlyContinue' 
-
-Copy-Item -Path "$env:USERPROFILE\documents\My Games\" -Destination "\\192.168.2.22\FTP\spielstaende\$env:USERNAME\" -Force -Recurse
-Copy-Item -Path "$env:USERPROFILE\documents\GTA San Andreas User Files\" -Destination "\\192.168.2.22\FTP\spielstaende\$env:USERNAME\" -Force -Recurse
+rm \\192.168.2.22\FTP\spielstaende\$env:USERNAME\* -Force
+Copy-Item -Path "$env:USERPROFILE\documents\*" -Destination "\\192.168.2.22\FTP\spielstaende\$env:USERNAME\" -Force -Recurse
