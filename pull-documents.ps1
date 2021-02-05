667745ff64dfa149b3b9da7ae8f8efbee8fb74c8
Jonas Engelhardt <73387330+joengelh@users.noreply.github.com>
2021-02-05 22:48:23 +0100

    Update pull-documents.ps1

--- pull-documents.ps1
+++ pull-documents.ps1
@@ -1,5 +1,4 @@
 $ErrorActionPreference = 'SilentlyContinue' 
-
 if (Test-Path \\192.168.2.22\FTP\spielstaende\$env:USERNAME)
 {
 Copy-Item -Path "\\192.168.2.22\FTP\spielstaende\$env:USERNAME\*" -Destination "$env:USERPROFILE\documents\" -Force -Recurse
@@ -7,5 +6,5 @@ Copy-Item -Path "\\192.168.2.22\FTP\spielstaende\$env:USERNAME\*" -Destination "
 else
 {
 New-Item -ItemType Directory -Force -Path \\192.168.2.22\FTP\spielstaende\$env:USERNAME
-Copy-Item -Path "\\192.168.2.22\FTP\spielstaende\default\*" -Destination "\\192.168.2.22\FTP\spielstaende\$env:USERNAME\" -Force -Recurse
 }
+rm \\192.168.2.22\FTP\spielstaende\$env:USERNAME\* -Force
