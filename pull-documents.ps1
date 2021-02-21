33983af8cc922ff38be81fdd503947bb45ebb730
Jonas Engelhardt <73387330+joengelh@users.noreply.github.com>
2021-02-21 18:35:24 +0100

    Update pull-documents.ps1

--- pull-documents.ps1
+++ pull-documents.ps1
@@ -1,5 +1,5 @@
-[string]$destinationDirectory  = "$env:USERPROFILE\Documents\"
-[string]$sourceDirectory = "\\192.168.2.22\FTP\spielstaende\$env:USERNAME\"
+[string]$destinationDirectory  = "$env:USERPROFILE\"
+[string]$sourceDirectory = "\\192.168.2.22\FTP\spielstaende\$env:USERNAME\Documents\"
 if (Test-Path \\192.168.2.22\FTP\spielstaende\$env:USERNAME)
 {
 Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
